c3ddb73264b6f24b5ea7474038570542a692a777
KRISHNA TRIPATHI <bktripathi1977@gmail.com>
2024-12-31 12:24:38 +0530

    Modified the business logic and endpoints as per Spring Security implementation.

--- src/main/java/com/intelliguru/simplora/repository/UserRepository.java
+++ src/main/java/com/intelliguru/simplora/repository/UserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends MongoRepository<User, ObjectId> {
     User findByUserName(String username);
+    void deleteByUserName(String username);
 }
--- src/main/java/com/intelliguru/simplora/service/UserService.java
+++ src/main/java/com/intelliguru/simplora/service/UserService.java
@@ -4,17 +4,29 @@ import com.intelliguru.simplora.entity.User;
 import com.intelliguru.simplora.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 @Component
 public class UserService {
+    private static final PasswordEncoder passwordencoder = new BCryptPasswordEncoder();
     @Autowired
     private UserRepository userRepository;
 
+    public void saveNewUser(User users) {
+        users.setPassword(passwordencoder.encode(users.getPassword()));
+        users.setRoles(List.of("USER"));
+        userRepository.save(users);
+    }
     public void saveUser(User users) {
+        users.setPassword(passwordencoder.encode(users.getPassword()));
+        users.setRoles(List.of("USER"));
         userRepository.save(users);
     }
 
